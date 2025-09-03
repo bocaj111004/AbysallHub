1a3f979a4cb05c3117ae665c818aa0e75e60da02
bocaj111004 <bocaj111004@gmail.com>
2025-09-03 13:43:22 +0100

    Update InfoTab.lua

--- Misc/InfoTab.lua
+++ Misc/InfoTab.lua
@@ -11,7 +11,7 @@ SupportedGames:AddLabel("[Universal]: Working", true)
 
   Extra:AddLabel(AbysallHubSettings.Name .. " is currently keyless, but may have a key system in the future depending on how things go", true)
   Extra:AddDivider()
-  Extra:AddLabel("Feel free to upload content using " .. AbysallHubSettings.Name .. " or upload it to scriptblox with no credit needed.")
+  Extra:AddLabel("Feel free to upload content using " .. AbysallHubSettings.Name .. " or upload it to scriptblox with no credit needed.", true)
 
 local Socials = Tab:AddRightGroupbox("Socials")
   Socials:AddButton({
@@ -28,11 +28,11 @@ local Socials = Tab:AddRightGroupbox("Socials")
 })
 
 local Credits = Tab:AddRightGroupbox("Contributors")
-  Credits:AddLabel("[bocaj11104]: Owner")
+  Credits:AddLabel("[bocaj11104]: Owner", true)
   Credits:AddDivider()
-  Credits:AddLabel("[thehuntersolo1]: Helped to script a few things, found exploits for Doors")
-  Credits:AddLabel("[feargeorge]: Helped to script a few things, gave me some ideas for features")
-  Credits:AddLabel("[mstudio45]: Made the ESP system we previously used (https://github.com/mstudio45/MSESP), before we made our own")
+  Credits:AddLabel("[thehuntersolo1]: Helped to script a few things, found exploits for Doors", true)
+  Credits:AddLabel("[feargeorge]: Helped to script a few things, gave me some ideas for features", true)
+  Credits:AddLabel("[mstudio45]: Made the ESP system we previously used (https://github.com/mstudio45/MSESP), before we made our own", true)
 end
 
 return InfoTab
